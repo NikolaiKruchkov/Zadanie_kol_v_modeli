ee9cb092b5b7d103cbb2fa7ae7c2b476c9c7ed00
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2022-02-04 16:48:55 +0300

    Add files via upload

---
+++ Zadanie_stairs_model/Main.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_stairs_model
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            List<Autodesk.Revit.DB.Architecture.Stairs> finstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Stairs)
+                .WhereElementIsNotElementType()
+                .Cast<Autodesk.Revit.DB.Architecture.Stairs>()
+                .ToList();
+            TaskDialog.Show("Stairs count", $"Количество лестниц в модели {finstances.Count}");
+            return Result.Succeeded;
+        }
+    }
+}
